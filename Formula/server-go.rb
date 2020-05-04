b9fc550562802b0f54818f640f2781e967a396eb
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-04 23:08:29 +0800

    Brew formula update for server-go version v1.1.36

--- Formula/server-go.rb
+++ Formula/server-go.rb
@@ -2,24 +2,24 @@
 class ServerGo < Formula
   desc "OpenIoTHub Server"
   homepage "http://github.com/OpenIoTHub"
-  version "1.1.35"
+  version "1.1.36"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/OpenIoTHub/server-go/releases/download/v1.1.35/server-go_1.1.35_Darwin_x86_64.tar.gz"
-    sha256 "fe0117f6d777a1c06bd9ae579e212fa9b1aa946ac1d61e2023e1fb16c7a55cfa"
+    url "https://github.com/OpenIoTHub/server-go/releases/download/v1.1.36/server-go_1.1.36_Darwin_x86_64.tar.gz"
+    sha256 "052bbe8d1674dd91b833f10635628de5bfab989cccc1fbdff9cca2b5d98b887f"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/OpenIoTHub/server-go/releases/download/v1.1.35/server-go_1.1.35_Linux_x86_64.tar.gz"
-      sha256 "c4ba24efda4b31bf19be0a8f2ed85347fcd4aee1ae1df3a2e7d62004faf10ad2"
+      url "https://github.com/OpenIoTHub/server-go/releases/download/v1.1.36/server-go_1.1.36_Linux_x86_64.tar.gz"
+      sha256 "60620788552db3ee4a14114f26d20d5178f102a3c2141e973300bfc63db26561"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/OpenIoTHub/server-go/releases/download/v1.1.35/server-go_1.1.35_Linux_arm64.tar.gz"
-        sha256 "4d7a930587048fae8cb9dbf87d73f82fb6c471468756b5a936fc5725946bb28c"
+        url "https://github.com/OpenIoTHub/server-go/releases/download/v1.1.36/server-go_1.1.36_Linux_arm64.tar.gz"
+        sha256 "390bc4f5e73fb0680ecd23e540f136d362759a1cec45699cd736af0f666a883d"
       else
-        url "https://github.com/OpenIoTHub/server-go/releases/download/v1.1.35/server-go_1.1.35_Linux_armv6.tar.gz"
-        sha256 "f5f94d728893bdfce3dadcc13adfedb2ab541dc36a091197157c1254ff8e141b"
+        url "https://github.com/OpenIoTHub/server-go/releases/download/v1.1.36/server-go_1.1.36_Linux_armv6.tar.gz"
+        sha256 "0b766b583a3bf986c93eeaa8640d46b03c6a07de5992963b1639493c93f176a0"
       end
     end
   end
