af1675242436b661d3e0e9f0f7821bf2e376d8ca
goreleaserbot <goreleaser@carlosbecker.com>
2020-05-13 13:28:40 +0800

    Brew formula update for gateway-go version v0.1.56

--- Formula/gateway-go.rb
+++ Formula/gateway-go.rb
@@ -2,24 +2,24 @@
 class GatewayGo < Formula
   desc "OpenIoTHub GateWay"
   homepage "http://github.com/OpenIoTHub"
-  version "0.1.55"
+  version "0.1.56"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/OpenIoTHub/gateway-go/releases/download/v0.1.55/gateway-go_0.1.55_Darwin_x86_64.tar.gz"
-    sha256 "bcc8f587747904b3c3cbee3b40991c73efea18c20a55671bbd62a906880ed57f"
+    url "https://github.com/OpenIoTHub/gateway-go/releases/download/v0.1.56/gateway-go_0.1.56_Darwin_x86_64.tar.gz"
+    sha256 "18b43944cf86c5d373f187851f280786f1f9903feac4004c7cd0fe5514525fbc"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/OpenIoTHub/gateway-go/releases/download/v0.1.55/gateway-go_0.1.55_Linux_x86_64.tar.gz"
-      sha256 "f3a47fc59ac5a3d5675a36ec8cae97fd23d14b8568c5847cf76123909871e23e"
+      url "https://github.com/OpenIoTHub/gateway-go/releases/download/v0.1.56/gateway-go_0.1.56_Linux_x86_64.tar.gz"
+      sha256 "409317368ef862e55aa0d199eb649e6e0630edd4cbfa193f52f01cdbfba5ddf1"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/OpenIoTHub/gateway-go/releases/download/v0.1.55/gateway-go_0.1.55_Linux_arm64.tar.gz"
-        sha256 "f575660a6b43dfaab7e900a145ea825f37d1c520edbe8e69a668abe5d878d299"
+        url "https://github.com/OpenIoTHub/gateway-go/releases/download/v0.1.56/gateway-go_0.1.56_Linux_arm64.tar.gz"
+        sha256 "90c7cedbddfae9e77bd823eb80272f547941eb4aef3140ec94924dc3104d8966"
       else
-        url "https://github.com/OpenIoTHub/gateway-go/releases/download/v0.1.55/gateway-go_0.1.55_Linux_armv6.tar.gz"
-        sha256 "d79c8df002700782e1fb97a64ddc32adfebbcc43374e82e0d3c71c73ab46035d"
+        url "https://github.com/OpenIoTHub/gateway-go/releases/download/v0.1.56/gateway-go_0.1.56_Linux_armv6.tar.gz"
+        sha256 "276f2598f236dbb9187c873bf4beb8b0c7804a7913b197b24a618a8d2b6dfc4b"
       end
     end
   end
