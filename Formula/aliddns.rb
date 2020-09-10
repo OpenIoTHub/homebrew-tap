901067b11c9a437f9d0ba77f2d25d63f4ae69ec4
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-10 02:04:11 -0500

    Brew formula update for aliddns version v0.0.2

--- Formula/aliddns.rb
+++ Formula/aliddns.rb
@@ -2,24 +2,24 @@
 class Aliddns < Formula
   desc "Aliyun ddns service"
   homepage "http://github.com/OpenIoTHub"
-  version "0.0.1"
+  version "0.0.2"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.1/aliddns_0.0.1_Darwin_x86_64.tar.gz"
-    sha256 "154003b832708553161060d61e7f37cc3bd27df28d1b38510eca270779f44849"
+    url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.2/aliddns_0.0.2_Darwin_x86_64.tar.gz"
+    sha256 "9bbba5cbf75a2e387e3ecffb53d1fed774833a5df974e8858048586098942944"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.1/aliddns_0.0.1_Linux_x86_64.tar.gz"
-      sha256 "8e5d0c1303a524fa92359ac6296b03ffc78e33d37494c59be39d6e83f418dc04"
+      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.2/aliddns_0.0.2_Linux_x86_64.tar.gz"
+      sha256 "291591f58b876a6b511a050d8a6e101f4e391804e386e55704ca14f91af16008"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.1/aliddns_0.0.1_Linux_arm64.tar.gz"
-        sha256 "32404255c72b387791d823f52831eb2461a285237ea93322b88f7985e7e17fe9"
+        url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.2/aliddns_0.0.2_Linux_arm64.tar.gz"
+        sha256 "117b632802381d522043a2bb74b1c3d7589296dcb40dd295f95b1c274923a577"
       else
-        url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.1/aliddns_0.0.1_Linux_armv6.tar.gz"
-        sha256 "2f8a87a34afd0807d2fe7f2e41086ef07e17cb87113fdc78dd1452075d3ce12e"
+        url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.2/aliddns_0.0.2_Linux_armv6.tar.gz"
+        sha256 "33b735ee2ef98cb236329d2fd3cc5eb29df0605be0b56622dd4e8331afd28e8b"
       end
     end
   end
