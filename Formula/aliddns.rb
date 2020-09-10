c0671ee373ad25c5bcd3bce11cf5173dd0a28983
goreleaserbot <goreleaser@carlosbecker.com>
2020-09-10 02:48:47 -0500

    Brew formula update for aliddns version v0.0.3

--- Formula/aliddns.rb
+++ Formula/aliddns.rb
@@ -2,24 +2,24 @@
 class Aliddns < Formula
   desc "Aliyun ddns service"
   homepage "http://github.com/OpenIoTHub"
-  version "0.0.2"
+  version "0.0.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.2/aliddns_0.0.2_Darwin_x86_64.tar.gz"
-    sha256 "9bbba5cbf75a2e387e3ecffb53d1fed774833a5df974e8858048586098942944"
+    url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.3/aliddns_0.0.3_Darwin_x86_64.tar.gz"
+    sha256 "024f5ee1c40246bdf0d32b499b46b12ffb4aa58e706c7620c0c9aba25fc0cdcb"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.2/aliddns_0.0.2_Linux_x86_64.tar.gz"
-      sha256 "291591f58b876a6b511a050d8a6e101f4e391804e386e55704ca14f91af16008"
+      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.3/aliddns_0.0.3_Linux_x86_64.tar.gz"
+      sha256 "f466656527d24aca08e136e2c43cc2759044fa7a4611235e91f0030d30ce1245"
     end
     if Hardware::CPU.arm?
       if Hardware::CPU.is_64_bit?
-        url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.2/aliddns_0.0.2_Linux_arm64.tar.gz"
-        sha256 "117b632802381d522043a2bb74b1c3d7589296dcb40dd295f95b1c274923a577"
+        url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.3/aliddns_0.0.3_Linux_arm64.tar.gz"
+        sha256 "02705c826cb979ea18d4741c5e78bb67f1c942495136e74ccb0669afc4ca5df2"
       else
-        url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.2/aliddns_0.0.2_Linux_armv6.tar.gz"
-        sha256 "33b735ee2ef98cb236329d2fd3cc5eb29df0605be0b56622dd4e8331afd28e8b"
+        url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.3/aliddns_0.0.3_Linux_armv6.tar.gz"
+        sha256 "25973cce96a380ed6fc15199c56f0633438418231923f778bada51372d2553f1"
       end
     end
   end
