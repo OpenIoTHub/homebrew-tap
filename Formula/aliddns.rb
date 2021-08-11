f34cc3b2ba82cb40fa3733d3ed95bcab11474732
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-11 11:48:12 +0800

    Brew formula update for aliddns version v0.0.11

--- Formula/aliddns.rb
+++ Formula/aliddns.rb
@@ -5,28 +5,28 @@
 class Aliddns < Formula
   desc "Aliyun ddns service"
   homepage "http://github.com/OpenIoTHub"
-  version "0.0.10"
+  version "0.0.11"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.10/aliddns_0.0.10_Darwin_x86_64.tar.gz"
-      sha256 "e3b66b46e86ac218d92ffb5873e9eabcc9187a25486ce88c2052f276afeb511f"
+      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.11/aliddns_0.0.11_Darwin_x86_64.tar.gz"
+      sha256 "dc704305bc6a48f23c5e82e2b95fc0d6329a14fc59adcc534fe9cda0eb9b14fd"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.10/aliddns_0.0.10_Linux_x86_64.tar.gz"
-      sha256 "2b0efbb971959c0edca114562209df05c649d5a2d5b99d1e8d9fe005b6a085a3"
+      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.11/aliddns_0.0.11_Linux_x86_64.tar.gz"
+      sha256 "99e2484c7efaa01b6a65ef2f67f82587592512c1dc3eb17f2c651780862c2bfb"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
-      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.10/aliddns_0.0.10_Linux_armv6.tar.gz"
-      sha256 "f36e55e5eb53bf8af1454a5fb73376f8d34a9158dee875c5ba8aeaa246b5d69a"
+      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.11/aliddns_0.0.11_Linux_armv6.tar.gz"
+      sha256 "127dfbe243e849f648c939f40f13534f767af0ee491aab310d3aa9b5ebdddf5f"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.10/aliddns_0.0.10_Linux_arm64.tar.gz"
-      sha256 "c6bdc940caebfb3499c0cb8a9ec48c2eff82c47235453697696966de047ca6ec"
+      url "https://github.com/OpenIoTHub/aliddns/releases/download/v0.0.11/aliddns_0.0.11_Linux_arm64.tar.gz"
+      sha256 "0b5521f025f7ae3129f76409e3c331c9d5ebd2753302a82ce61051e46f99968a"
     end
   end
 
